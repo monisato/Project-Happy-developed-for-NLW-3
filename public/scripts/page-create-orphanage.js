75adca8f4410ca11d313011fbfc2addb619f264a
unknown <monicasakane@gmail.com>
2020-10-20 12:05:23 -0300

    teste phone number pattern

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -114,6 +114,7 @@ function validate(event) {
   } 
 }
 
+// format phone number input
 /*function phoneMask() { 
   var num = $(this).val().replace(/\D/g,''); 
   $(this).val('(' + num.substring(0,2) + ')' + num.substring(2,7) + '-' + num.substring(7,11)); 
